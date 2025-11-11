e9af455954439d6bfbe32f06f62f7636650471db
Kim Sanghae <pelikan@pusan.ac.kr>
2025-11-11 20:02:47 +0900

    Initial Commit

---
+++ LottoApp/LottoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LottoAppApp.swift
+//  LottoApp
+//
+//  Created by 김상해 on 11/11/25.
+//
+
+import SwiftUI
+
+@main
+struct LottoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
