53d1a4b23f85b9427c0fb66850b75fff5d0634d1
Kim Sanghae <pelikan@pusan.ac.kr>
2025-11-20 19:34:30 +0900

    feat: Coordinator 생성
    - metaDataOutput을 통해 결과 도출
    - 2초 내에 동일 결과라면 다른 QR이 아닌 동일 QR 코드의 연속으로 봄

--- LottoApp/Views/QRScannerView.swift
+++ LottoApp/Views/QRScannerView.swift
@@ -17,4 +17,35 @@ struct QRScannerView: UIViewControllerRepresentable {
     func updateUIViewController(_ uiViewController: ScannerViewController, context: Context) {
         // 뷰 업데이트 로직 (여기선 불필요)
     }
+    
+    // MARK: - Coordinator (중개인)
+        class Coordinator: NSObject, AVCaptureMetadataOutputObjectsDelegate {
+            let parent: QRScannerView
+            var lastScannedCode: String?
+            var lastScanTime: Date = Date()
+            
+            init(parent: QRScannerView) {
+                self.parent = parent
+            }
+            
+            func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+                
+                if let metadataObject = metadataObjects.first as? AVMetadataMachineReadableCodeObject,
+                   let scannedValue = metadataObject.stringValue {
+                    
+                    let now = Date()
+                    if scannedValue == lastScannedCode && now.timeIntervalSince(lastScanTime) < 2.0 {
+                        return
+                    }
+                    
+                    lastScannedCode = scannedValue
+                    lastScanTime = now
+                    
+                    AudioServicesPlaySystemSound(SystemSoundID(kSystemSoundID_Vibrate))
+                    
+                    parent.onScan(scannedValue)
+                }
+            }
+        }
+    }
 }
