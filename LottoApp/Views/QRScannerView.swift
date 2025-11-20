4898a357410cbf31d9422b8555545ce00984ecab
Kim Sanghae <pelikan@pusan.ac.kr>
2025-11-20 19:08:50 +0900

    feat: QRScannerView 생성

--- LottoApp/Views/QRScannerView.swift
+++ LottoApp/Views/QRScannerView.swift
@@ -1,8 +1,20 @@
-//
-//  QRScannerView.swift
-//  LottoApp
-//
-//  Created by 김상해 on 11/20/25.
-//
+import SwiftUI
+import AVFoundation
 
-import Foundation
+struct QRScannerView: UIViewControllerRepresentable {
+    var onScan: (String) -> Void
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(parent: self)
+    }
+    
+    func makeUIViewController(context: Context) -> ScannerViewController {
+        let controller = ScannerViewController()
+        controller.delegate = context.coordinator
+        return controller
+    }
+    
+    func updateUIViewController(_ uiViewController: ScannerViewController, context: Context) {
+        // 뷰 업데이트 로직 (여기선 불필요)
+    }
+}
